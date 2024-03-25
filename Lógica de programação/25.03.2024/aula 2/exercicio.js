9873df0e2c2b5ef18781278828e8e2124cfaea91
Alisson <alisson.fbernardo@gmail.com>
2024-03-25 17:03:37 -0300

    envio logica programa

---
+++ Lógica de programação/25.03.2024/aula 2/exercicio.js
@@ -0,0 +1,7 @@
+
+let nome = prompt("Qual é seu nome? ")
+
+let sobrenome = prompt("Qual é seu sobrenome? ")
+
+let nomeCompleto = nome + " " + sobrenome
+alert ("seu nome completo é: " + nomeCompleto)
