1a156292e0e565479778634ac84e6e292237ec5f
Alisson <alisson.fbernardo@gmail.com>
2024-03-25 17:11:02 -0300

    envio logica programa

--- Lógica de programação/25.03.2024/aula 2/exercicio.js
+++ Lógica de programação/25.03.2024/aula 2/exercicio.js
@@ -5,3 +5,11 @@ let sobrenome = prompt("Qual é seu sobrenome? ")
 
 let nomeCompleto = nome + " " + sobrenome
 alert ("seu nome completo é: " + nomeCompleto)
+
+/* 
+passos para criar esse algoritmo:
+
+1º ler o nome
+2º ler o sobrenome
+3º criar o nome completo juntando as variaves "nome" e "sobrenome"
+*/
