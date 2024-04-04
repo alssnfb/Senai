204ca990518ae3f83e31e0e4458052d3f7c47851
Alisson <alisson.fbernardo@gmail.com>
2024-04-04 14:03:47 -0300

    envio atividade lógica

--- Lógica de programação/03.04.2024/exercicios/exercicio for/exercicio for-3.js
+++ Lógica de programação/03.04.2024/exercicios/exercicio for/exercicio for-3.js
@@ -1,5 +1,7 @@
-let numeroTabuada = prompt("Digite o número da tabuada")
+let numeroTabuada = Number(prompt("Digite o número desejado para tabuada"))
+let i = 0
 
 for (let i=0; i <=20; i++) {
-    
+    let Resultado = i * numeroTabuada
+    console.log(numeroTabuada + " x " + i + " = " + Resultado)    
 }
