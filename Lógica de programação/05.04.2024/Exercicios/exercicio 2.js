db9beea5ef6c60deffe23a83c6bcc2148464c243
Alisson <alisson.fbernardo@gmail.com>
2024-04-05 15:50:59 -0300

    envio aula lógica sérgio

---
+++ Lógica de programação/05.04.2024/Exercicios/exercicio 2.js
@@ -0,0 +1,24 @@
+/* Escreva um código JS que peça dois números Mosre um menu com as opções:
+(1) Adição +,
+(2) Subtração -.
+(3) Divisão /,
+(4) Multiplicação *, e retorne a operação matemática*/
+
+let numeroUsuário1 = Number(prompt("Digite um número para operação"))
+let numeroUsuário2 = Number(prompt("Digite um segundo número para operação"))
+
+alert("Escolha uma operação: \n" +"(1) Adição \n" + "(2) Subtração \n" + "(3) Divisão \n" + "(4) Multiplicação" )
+switch(numeroUsuário1){
+case 1
+    alert(numeroUsuário1 + numeroUsuário2)
+    break
+case 2
+    alert(numeroUsuário1 - numeroUsuário2)
+    break
+case 3
+    alert(numeroUsuário1 / numeroUsuário2)
+    break
+case 4            
+    alert
+    
+}
