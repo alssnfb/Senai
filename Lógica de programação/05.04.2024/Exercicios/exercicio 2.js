68bfdf80eb85ced224f573fe2eaad978f0ddc3c2
Alisson <alisson.fbernardo@gmail.com>
2024-04-05 15:52:32 -0300

    envio aula lógica sérgio

--- Lógica de programação/05.04.2024/Exercicios/exercicio 2.js
+++ Lógica de programação/05.04.2024/Exercicios/exercicio 2.js
@@ -9,16 +9,17 @@ let numeroUsuário2 = Number(prompt("Digite um segundo número para operação")
 
 alert("Escolha uma operação: \n" +"(1) Adição \n" + "(2) Subtração \n" + "(3) Divisão \n" + "(4) Multiplicação" )
 switch(numeroUsuário1){
-case 1
+case 1:
     alert(numeroUsuário1 + numeroUsuário2)
     break
-case 2
+case 2:
     alert(numeroUsuário1 - numeroUsuário2)
     break
-case 3
+case 3:
     alert(numeroUsuário1 / numeroUsuário2)
     break
-case 4            
-    alert
+case 4:            
+    alert(numeroUsuário1 * numeroUsuário2)
+    break
     
 }
