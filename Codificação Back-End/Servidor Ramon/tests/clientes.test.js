83d95de3cd0a78873b92088425dbbe8097b0683a
Alisson <alisson.fbernardo@gmail.com>
2024-09-02 15:18:58 -0300

    envio atividades

--- Codificação Back-End/Servidor Ramon/tests/clientes.test.js
+++ Codificação Back-End/Servidor Ramon/tests/clientes.test.js
@@ -1,13 +1,22 @@
 const request = require('supertest')
 const app = require('../index')
 
-describe('GET /cliente', () =>{
-    it('pegar a lista de clientes com sucesso', async() =>{
-        const res = await request(app).get('/cliente').send();
-        expect(res.status).toBe(200);
+describe('GET /cliente', () => {
+    it('pegar a lista de clientes com sucesso', async () => {
+        const res = await request(app).get('/cliente').send()
+        expect(res.status).toBe(200)
     })
-    it('verificar se a lista de clientes esta cheia', async() =>{
-        const res = await request(app).get('/cliente').send();
-        expect(res.body).toBeDefined();
+    it('verificar se a lista de clientes esta cheia', async () => {
+        const res = await request(app).get('/cliente').send()
+        expect(res.body).toBeDefined()
     })
-});
+})
+
+describe('Atualizar /cliente/:id', () => {
+    it('Atualizar nome do cliente com sucesso', async() =>{
+        const res = await request(app).put('/cliente/c2da497b-c598-461a-a26a-d003ff23f392').send({
+            nome: 'ramon update'
+        })
+        expect(res.status).toBe(204)
+    })
+})
