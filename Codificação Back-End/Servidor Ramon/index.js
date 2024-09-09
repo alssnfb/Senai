69eb8fb09d7bad63320d97723657ac9f03ab6cd6
Alisson <alisson.fbernardo@gmail.com>
2024-09-09 17:06:29 -0300

    envio correção atividade do ramon

--- Codificação Back-End/Servidor Ramon/index.js
+++ Codificação Back-End/Servidor Ramon/index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const rotas_produtos = require('./rotas/rotas_produtos')
 const rotas_clientes = require('./rotas/rotas_clientes')
+const rotas_autenticacao = require('./rotas/rotas_autenticacao')
 
 
 app.use(bodyParser.json())
@@ -11,6 +12,8 @@ app.use('/produtos', rotas_produtos)
 
 app.use('/cliente', rotas_clientes)
 
-// app.listen(3340)
+app.use('/auth', rotas_autenticacao)
+
+app.listen(3340)
 
 module.exports = app;
